6f979fd9d33f537854d376e410e84cd785016f89
HOWISYUNI <blissoooooo@gmail.com>
2020-07-28 01:01:31 +0900

    use Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Subject extends Component{
+  render(){
+    return(
+      <header>
+          <h1>React Tutorial</h1>
+      </header>
+    );
+  }
+}
+
+class TOC extends Component{
+  render(){
+    return(
+      <nav>
+        <ul>
+          <li><a href="1.html">Component</a></li>
+          <li><a href="2.html">State</a></li>
+          <li><a href="3.html">Event</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component{
+  render(){
+    return(
+      <article>
+        Component article. Tools making React coding easier than what I used do.
+      </article> 
+    );
+  }
+}
 
 class App extends Component{
   render() {
     return (
       <div className="App">
-        Hello react!!
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
       </div>
     );
   }
